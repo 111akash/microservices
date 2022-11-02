54f8f1bf0caf442bea0a49ddb5a1721b3bd38f79
akash <akash329@protonmail.com>
2022-11-02 20:26:55 +0530

    added project files

---
+++ department-service/department-service/src/main/java/com/microserviceslearning/department/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.microserviceslearning.department.repository;
+
+import com.microserviceslearning.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentId(Long departmentId);
+}
